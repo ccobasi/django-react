fc14a9b44ff01144a9b38f2c8e58e62fcc8eb799
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-11-11 21:52:39 +0100

    fix bug

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
